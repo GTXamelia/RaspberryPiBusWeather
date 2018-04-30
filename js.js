e8e58bbdef763247b186d4af61eb7ebf06ca30ad
cian2009 <g00337022@gmit.ie>
2018-04-30 13:52:01 +0100

    Small update

--- js.js
+++ js.js
@@ -10,6 +10,11 @@ window.onload = function what() {
     var mydiv = document.getElementById("londis");
     var br = document.createElement("br");
 
+    var table = document.getElementById("bus1");
+
+    var header = table.createTHead();
+    
+
     for (var i = 0; i < numberofresults; i++) {
       var buslocal = bus[i].origin.split(" ")[0] + " " + bus[i].route;
       var due = bus[i].duetime;
@@ -17,12 +22,27 @@ window.onload = function what() {
 
       console.log("Bus: " + buslocal + "\nDue: " + due + "\nDestination: " + destination);
 
-      mydiv.appendChild(document.createTextNode("Bus: " + buslocal + "\nDue: " + due + "\nDestination: " + destination));
+      var row = table.insertRow(i);
 
-     
-      
+      // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
+      var cell1 = row.insertCell(0);
+      var cell2 = row.insertCell(1);
+      var cell3 = row.insertCell(2);
+
+      cell1.innerHTML = buslocal;
+      cell2.innerHTML = destination;
+      cell3.innerHTML = due;
+
 
     }
+
+    var row = header.insertRow(0);
+    var head1 = row.insertCell(0);
+    var head2 = row.insertCell(1);
+    var head3 = row.insertCell(2);
+    head1.innerHTML = "<b>Bus</b>";
+    head2.innerHTML = "<b>Destination</b>";
+    head3.innerHTML = "<b>Due</b>";
 
   });
 };
