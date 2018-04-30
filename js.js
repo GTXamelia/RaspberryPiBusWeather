3f45cbb4cb303083d5b1f5a53c023f170c7e8339
cian2009 <g00337022@gmit.ie>
2018-04-30 12:45:42 +0100

    Custom loop depending on results

--- js.js
+++ js.js
@@ -1,12 +1,17 @@
 window.onload = function what() {
-  $.getJSON("https://data.smartdublin.ie/cgi-bin/rtpi/realtimebusinformation?stopid=522961&format=json", function(data) {
+  $.getJSON("https://data.smartdublin.ie/cgi-bin/rtpi/realtimebusinformation?stopid=522961&format=json", function (data) {
     console.log(data);
 
-      // figuring out timestamp, time expected of train, and then difference, then converting to rounded minutes
-      var bus = data.results;
-      console.log(bus);
+
+
+
+    var numberofresults = data.numberofresults;
+    console.log(numberofresults);
+    var bus = data.results;
+    console.log(bus);
+
+    for (var i = 0; i < numberofresults; i++) {
+      console.log(i);
+    }
   });
 };
-
-// Method to look at next time for date formats.
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toISOString
