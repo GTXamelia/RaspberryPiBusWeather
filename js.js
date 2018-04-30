e155ddf1ef8f1d8e4663d1a0155db9deee1ade57
Cian Gannon <lego@live.ie>
2018-04-30 18:37:25 +0100

    Added functions to handle duplicate code

--- js.js
+++ js.js
@@ -8,39 +8,10 @@ window.onload = function what() {
     var header = table.createTHead();
 
     for (var i = 0; i < numberofresults; i++) {
-      var buslocal = "(" + bus[i].route + ") " + bus[i].origin.split(" ")[0];
-      var due = bus[i].duetime;
-      var destination = bus[i].destination;
-
-      if (due == "Due") {
-
-      } else if (due == 1) {
-        due = due + " Minute";
-      } else {
-        due = due + " Minutes";
-      }
-
-      console.log("Bus: " + buslocal + "\nDue: " + due + "\nDestination: " + destination);
-
-      var row = table.insertRow(i);
-
-      var cell1 = row.insertCell(0);
-      var cell2 = row.insertCell(1);
-      var cell3 = row.insertCell(2);
-
-      cell1.innerHTML = buslocal;
-      cell2.innerHTML = destination;
-      cell3.innerHTML = due;
-
+      
+      addRow(i, table, header, bus, numberofresults);
+      
     }
-
-    var row = header.insertRow(0);
-    var head1 = row.insertCell(0);
-    var head2 = row.insertCell(1);
-    var head3 = row.insertCell(2);
-    head1.innerHTML = "<b>Bus</b>";
-    head2.innerHTML = "<b>Destination</b>";
-    head3.innerHTML = "<b>Due</b>";
   });
 
   $.getJSON("https://data.smartdublin.ie/cgi-bin/rtpi/realtimebusinformation?stopid=522691&format=json", function (data) {
@@ -55,15 +26,7 @@ window.onload = function what() {
       var due = bus[i].duetime;
       var destination = bus[i].destination;
 
-      if (due == "Due") {
-        due = due + " Soon"
-      } else if (due == 1) {
-        due = due + " Minute";
-      } else {
-        due = due + " Minutes";
-      }
-
-      console.log("Bus: " + buslocal + "\nDue: " + due + "\nDestination: " + destination);
+      due = dueFunc(due);
 
       var row = table.insertRow(i);
 
@@ -87,3 +50,47 @@ window.onload = function what() {
 
   });
 };
+
+function dueFunc(due) {
+
+  if (due == "Due") {
+    due = due + " Soon"
+  } else if (due == 1) {
+    due = due + " Minute";
+  } else {
+    due = due + " Minutes";
+  }
+
+  return due;
+}
+
+function addRow(i, table, header, bus, numberofresults) {
+
+  var buslocal = "(" + bus[i].route + ") " + bus[i].origin.split(" ")[0];
+  var due = bus[i].duetime;
+  var destination = bus[i].destination;
+
+  var row = table.insertRow(i);
+
+  due = dueFunc(due);
+
+  var cell1 = row.insertCell(0);
+  var cell2 = row.insertCell(1);
+  var cell3 = row.insertCell(2);
+
+  cell1.innerHTML = buslocal;
+  cell2.innerHTML = destination;
+  cell3.innerHTML = due;
+
+  if (i+1 >= numberofresults){
+
+    var row = header.insertRow(0);
+    var head1 = row.insertCell(0);
+    var head2 = row.insertCell(1);
+    var head3 = row.insertCell(2);
+    head1.innerHTML = "<b>Bus</b>";
+    head2.innerHTML = "<b>Destination</b>";
+    head3.innerHTML = "<b>Due</b>";
+
+  }
+}
