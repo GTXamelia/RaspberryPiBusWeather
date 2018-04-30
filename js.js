5f218a0aaae75b1d48f67eb9182d22ef072910fe
Cian Gannon <lego@live.ie>
2018-04-30 18:40:59 +0100

    Small formatting

--- js.js
+++ js.js
@@ -1,4 +1,4 @@
-window.onload = function what() {
+window.onload = function load() {
   $.getJSON("https://data.smartdublin.ie/cgi-bin/rtpi/realtimebusinformation?stopid=522961&format=json", function (data) {
 
     var numberofresults = data.numberofresults;
@@ -10,7 +10,7 @@ window.onload = function what() {
     for (var i = 0; i < numberofresults; i++) {
       
       addRow(i, table, header, bus, numberofresults);
-      
+
     }
   });
 
@@ -22,32 +22,10 @@ window.onload = function what() {
     var header = table.createTHead();
 
     for (var i = 0; i < numberofresults; i++) {
-      var buslocal = "(" + bus[i].route + ") " + bus[i].destination.split(" ")[0];
-      var due = bus[i].duetime;
-      var destination = bus[i].destination;
 
-      due = dueFunc(due);
-
-      var row = table.insertRow(i);
-
-      var cell1 = row.insertCell(0);
-      var cell2 = row.insertCell(1);
-      var cell3 = row.insertCell(2);
-
-      cell1.innerHTML = buslocal;
-      cell2.innerHTML = destination;
-      cell3.innerHTML = due;
+      addRow(i, table, header, bus, numberofresults);
 
     }
-
-    var row = header.insertRow(0);
-    var head1 = row.insertCell(0);
-    var head2 = row.insertCell(1);
-    var head3 = row.insertCell(2);
-    head1.innerHTML = "<b>Bus</b>";
-    head2.innerHTML = "<b>Destination</b>";
-    head3.innerHTML = "<b>Due</b>";
-
   });
 };
 
@@ -82,7 +60,7 @@ function addRow(i, table, header, bus, numberofresults) {
   cell2.innerHTML = destination;
   cell3.innerHTML = due;
 
-  if (i+1 >= numberofresults){
+  if (i+1 == numberofresults){
 
     var row = header.insertRow(0);
     var head1 = row.insertCell(0);
