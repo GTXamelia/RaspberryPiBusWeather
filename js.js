9f1d8333c5c00f792d43d9f46b65723cf562fa38
cian2009 <g00337022@gmit.ie>
2018-04-30 14:15:59 +0100

    Css changes

--- js.js
+++ js.js
@@ -1,22 +1,16 @@
 window.onload = function what() {
   $.getJSON("https://data.smartdublin.ie/cgi-bin/rtpi/realtimebusinformation?stopid=522961&format=json", function (data) {
-    console.log(data);
+
 
     var numberofresults = data.numberofresults;
-    console.log(numberofresults);
     var bus = data.results;
-    console.log(bus);
-
-    var mydiv = document.getElementById("londis");
-    var br = document.createElement("br");
 
     var table = document.getElementById("bus1");
-
     var header = table.createTHead();
     
 
     for (var i = 0; i < numberofresults; i++) {
-      var buslocal = bus[i].origin.split(" ")[0] + " " + bus[i].route;
+      var buslocal = "("+ bus[i].route + ") " + bus[i].origin.split(" ")[0];
       var due = bus[i].duetime;
       var destination = bus[i].destination;
 
