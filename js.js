a5e8167e60da633f8182f82a4e4f8ed8fcfd9483
cian2009 <g00337022@gmit.ie>
2018-04-30 12:41:00 +0100

    Got bus info from json

--- js.js
+++ js.js
@@ -1,6 +1,10 @@
 window.onload = function what() {
   $.getJSON("https://data.smartdublin.ie/cgi-bin/rtpi/realtimebusinformation?stopid=522961&format=json", function(data) {
     console.log(data);
+
+      // figuring out timestamp, time expected of train, and then difference, then converting to rounded minutes
+      var bus = data.results;
+      console.log(bus);
   });
 };
 
